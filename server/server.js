9d5c9d526512bc8f963be22bbcfff2fb6615b528
NOURI-Abdellah <a.nouri@esi-sba.dz>
2026-03-07 12:47:40 +0100

    creat server.js in main with Express server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+//start server
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, IhsanTrack!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
